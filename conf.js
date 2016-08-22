ae3f85a54a07c6e34a48381e2279abdcc253aae5
sumitroy2011 <sroy@sapient.com>
2016-08-22 17:00:13 +0100

    Change in conf.js URL

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@
 exports.config = {
   directConnect: true,
   specs: ['specs/*.js'],
-  baseUrl: 'https://christmasfood.marksandspencer.com/' ,
+  baseUrl: 'https://christmasfood.marksandspencer.com/collection' ,
   // beforeLaunch: function()  {
   //         console.log('beforeLaunch');
   //         // browser.driver.get('www.google.com');
